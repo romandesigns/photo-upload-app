e31426437f33ccdd774de5dbaa4c8cea0e20ff7a
Roman F. <wavystyledev@gmail.com>
2024-06-01 12:47:53 -0400

    installed supabase locally and unpdated .env files

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
   images: {
     domains: ["img.daisyui.com"],
   },
+  env: {
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+  },
 };
 
 export default nextConfig;
